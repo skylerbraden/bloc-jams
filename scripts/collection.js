d7f95f61ee0943d1c04289d9d01524f3d9585362
Skyler Braden <skylerbraden@me.com>
2015-12-16 12:13:19 -0600

    Checkpoint 30 jQuery Collection and Album Views, completed

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,31 +1,34 @@
-var collectionItemTemplate =
-     '<div class="collection-album-container column fourth">'
-   + '  <img src="assets/images/album_covers/01.png"/>'
-   + '  <div class="collection-album-info caption">'
-   + '    <p>'
-   + '      <a class="album-name" href="/album.html"> The Colors </a>'
-   + '      <br/>'
-   + '      <a href="/album.html"> Pablo Picasso </a>'
-   + '      <br/>'
-   + '      X songs'
-   + '      <br/>'
-   + '    </p>'
-   + '  </div>'
-   + '</div>'
-   ;
+// #1
+var buildCollectionItemTemplate = function() {
+    var template = 
+         '<div class="collection-album-container column fourth">'
+       + '  <img src="assets/images/album_covers/01.png"/>'
+       + '  <div class="collection-album-info caption">'
+       + '    <p>'
+       + '      <a class="album-name" href="/album.html"> The Colors </a>'
+       + '      <br/>'
+       + '      <a href="/album.html"> Pablo Picasso </a>'
+       + '      <br/>'
+       + '      X songs'
+       + '      <br/>'
+       + '    </p>'
+       + '  </div>'
+       + '</div>'
+       ;
+    
+    // #2 Here, we wrap template in a jQuery object (even though we don't use any jQuery methods, we may later) to future-proof it.
+    return $(template);
+};
 
-window.onload = function() {
-    
-    // #1
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
-    
-    // #2
-    collectionContainer.innerHTML = '';
-    
-    
-    
+$(window).load(function() {  
     // #3
+    var $collectionContainer = $('.album-covers');
+    // #4
+    $collectionContainer.empty();
+    
     for (var i = 0; i < 12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
+        var $newThumbnail = buildCollectionItemTemplate();
+        // #5
+        $collectionContainer.append($newThumbnail);
     }
-}
+});
